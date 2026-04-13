d30e557ce2760467dd8c1512ac354ed8f9e99f32
BZLZHH <admin@bzlzhh.top>
2026-04-13 11:54:37 +0800

    [Improvement] (Texture): Remove BGRA handling for 1.12.2

--- SimpleFPEWrapper/drawing.cpp
+++ SimpleFPEWrapper/drawing.cpp
@@ -30,35 +30,3 @@ void glDrawArrays(GLenum mode, GLint first, GLsizei count) {
     g_glFuncs.glBindVertexArray(0);
 }
 
-void glTexImage2D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLsizei height, GLint border,
-                  GLenum format, GLenum type, const GLvoid* pixels) {
-    // Fix for 1.12
-    if (format == GL_BGRA && type == GL_UNSIGNED_INT_8_8_8_8_REV) {
-        internalFormat = GL_BGRA;
-        format = GL_BGRA;
-        type = GL_UNSIGNED_BYTE;
-    }
-    g_glFuncs.glTexImage2D(target, level, internalFormat, width, height, border, format, type, pixels);
-}
-
-void glTexSubImage2D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height,
-                     GLenum format, GLenum type, const void* pixels) {
-    if (format == GL_BGRA && (type == GL_UNSIGNED_INT_8_8_8_8)) {
-        format = GL_RGBA;
-        type = GL_UNSIGNED_BYTE;
-    }
-    if (format == GL_BGRA && type == GL_UNSIGNED_INT_8_8_8_8_REV) {
-        type = GL_UNSIGNED_BYTE;
-    }
-
-    g_glFuncs.glTexSubImage2D(target, level, xoffset, yoffset, width, height, format, type, pixels);
-}
-
-void glReadPixels(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, void* pixels) {
-
-    if (format == GL_BGRA && type == GL_UNSIGNED_INT_8_8_8_8) {
-        format = GL_RGBA;
-        type = GL_UNSIGNED_BYTE;
-    }
-    g_glFuncs.glReadPixels(x, y, width, height, format, type, pixels);
-}
